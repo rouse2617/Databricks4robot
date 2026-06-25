01ac21197aaae051ad1c1ed6a578f7d99ef9b485
ruipeng.huang <ruipeng.huang@cyberorigin.ai>
2026-06-25 18:24:23 +0800

    chore: update SDK version file

--- sdk/src/cyber_databrew_sdk/_version.py
+++ sdk/src/cyber_databrew_sdk/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.1.dev335'
-__version_tuple__ = version_tuple = (0, 0, 1, 'dev335')
+__version__ = version = '0.0.1.dev148'
+__version_tuple__ = version_tuple = (0, 0, 1, 'dev148')
 
 __commit_id__ = commit_id = None
